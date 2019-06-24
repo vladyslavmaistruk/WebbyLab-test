753bb082309dc991d17270602bdd9801db183339
Vladyslav Maistruk <vladyslav.maistruk@gmail.com>
2019-06-25 01:39:40 +0300

    inheritance

--- testES5.js
+++ testES5.js
@@ -1,16 +1,4 @@
-/*
-Написать класс Dog, который наследуется от класcа Animal.
-Класс Animal имеет метод getName (name можно передать в конструктор).
-Класс Dog имеет метод bark (возвращает строку «Dog {dogname} is barking».
 
-Пример использования:
-
-var dog = new Dog ('Aban');
-dog.getName () === 'Aban'; // true
-dog.bark () === 'Dog Aban is barking'; // true
-
-Задание необходимо сделать в двух вариантах: на ES5 и ES6.
-*/
 
 function Animal (name) {
   this.name = name;
